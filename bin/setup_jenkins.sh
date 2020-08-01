16a9d041cc05d972c59467c8c5e13c7e12a4b550
srinivas.sarat.yadavalli-ibm.com <srinivas.sarat.yadavalli@ibm.com>
2020-08-01 00:56:57 +0000

    Update 'bin/setup_jenkins.sh'

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -16,9 +16,27 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # DO NOT FORGET TO PASS '-n ${GUID}-jenkins to ALL commands!!'
 # You do not want to set up things in the wrong project.
 
-# TBD
+oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
+oc set resources dc jenkins --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=500m -n ${GUID}-jenkins
+
+oc new-build --strategy=docker -D $'FROM registry.access.redhat.com/ubi8/go-toolset:latest as builder\n
+ENV SKOPEO_VERSION=v1.0.0\n
+RUN git clone -b $SKOPEO_VERSION https://github.com/containers/skopeo.git && cd skopeo/ && make binary-local DISABLE_CGO=1\n
+FROM image-registry.openshift-image-registry.svc:5000/openshift/jenkins-agent-maven:v4.0 as final\n
+USER root\n
+RUN mkdir /etc/containers\n
+COPY --from=builder /opt/app-root/src/skopeo/default-policy.json /etc/containers/policy.json\n
+COPY --from=builder /opt/app-root/src/skopeo/skopeo /usr/bin\n
+USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
+
+oc create secret generic gitea-secret --from-literal=username=srinivas.sarat.yadavalli-ibm.com --from-literal=password=Adm1n1strat0r -n ${GUID}-jenkins
+#oc set build-secret --source bc/tasks gitea-secret -n ${GUID}-jenkins
 
 
+oc new-app --template=eap72-basic-s2i --param APPLICATION_NAME=tasks --param SOURCE_REPOSITORY_URL=https://homework-gitea.apps.shared-na4.na4.openshift.opentlc.com/SaratOCPHW/openshift-tasks.git --param SOURCE_REPOSITORY_REF=master --param CONTEXT_DIR=openshift-tasks --param MAVEN_MIRROR_URL=https://homework-nexus.apps.shared-na4.na4.openshift.opentlc.com/repository/maven-all-public/ -n ${GUID}-jenkins
+
+#oc create secret generic gitea-secret --from-literal=username=srinivas.sarat.yadavalli-ibm.com --from-literal=password=Adm1n1strat0r -n ${GUID}-jenkins
+oc set build-secret --source bc/tasks gitea-secret -n ${GUID}-jenkins
 # Create custom agent container image with skopeo.
 # Build config must be called 'jenkins-agent-appdev' for the test below to succeed
 
@@ -43,7 +61,7 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 
 
 # Set up ConfigMap with Jenkins Agent definition
-oc create -f ./manifests/agent-cm.yaml -n ${GUID}-jenkins
+oc create -f ../manifests/agent-cm.yaml -n ${GUID}-jenkins
 
 # ========================================
 # No changes are necessary below this line
@@ -59,6 +77,8 @@ while : ; do
   sleep 10
 done
 
+oc start-build tasks -n ${GUID}-jenkins
+
 # Make sure that Jenkins Agent Build Pod has finished building
 while : ; do
   echo "Checking if Jenkins Agent Build Pod has finished building..."
@@ -70,3 +90,9 @@ while : ; do
   echo "...no. Sleeping 10 seconds."
   sleep 10
 done
+oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:homework-jenkins -n ${GUID}-jenkins
+oc policy add-role-to-user admin system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-tasks-dev
+oc policy add-role-to-user admin system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-tasks-prod
+oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-tasks-dev
+oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-tasks-prod
+oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-tasks-prod -n ${GUID}-tasks-dev
